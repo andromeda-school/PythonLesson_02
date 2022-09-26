56264073ce53a8a299fd06252a1d252e78899e67
Алмас Синев <114406236+andromeda-school@users.noreply.github.com>
2022-09-26 15:44:45 +0300

    Урок #2 по Python

---
+++ main.py
@@ -0,0 +1,37 @@
+# Вспомним, какие типы данных есть в Python
+number = 1  # int (integer)
+float_number = 3.14  # double (float)
+text = "Привет, Мир!"  # string
+text2 = 'Привет мир 2 !'  # тоже string
+key = True  # boolean
+key2 = False  # boolean
+
+# Поиграем с числами
+num1 = 34
+num2 = 2
+print("34 / 5 = ", num1 / num2)
+print("34 // 5 = ", num1 // num2)
+print("34 % 5 = ", num1 % num2)
+
+# Демонстрация специальных символов в выводе
+print('What\'s Up\n\n')
+
+# Проверим, что нам выдает программа
+#  если мы попробуем сравнить числа
+print(3 > 1)
+print(3 < 1)
+print("-----------")  # это для разделения результатов
+print(3 == 1)
+print(3 != 1)
+
+#
+# Кусок для тех, кому интересно
+# работать с строками
+#
+# text_1 = "Python IS GREAT !"
+# arr1 = text_1.split(" ")
+# print(arr1)
+# arr2 = ["Hello", "World", "!"]
+# text2 = " ".join(arr2)
+#
+# print(text2)
